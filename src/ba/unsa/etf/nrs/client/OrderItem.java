378c20be72af9ee8039af951a554d17e95203f1a
mgojak1 <mgojak1@etf.unsa.ba>
2020-05-30 18:04:29 +0200

    OrderItem Model Added

---
+++ src/ba/unsa/etf/nrs/client/OrderItem.java
@@ -0,0 +1,44 @@
+package ba.unsa.etf.nrs.client;
+
+public class OrderItem {
+    int id, quantity;
+    Product product;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(int id, int quantity, Product product) {
+        this.id = id;
+        this.quantity = quantity;
+        this.product = product;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "orderItem#" + getId();
+    }
+}
